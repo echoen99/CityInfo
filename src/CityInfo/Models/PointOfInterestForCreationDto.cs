fead051c91ca0b8689b5a942b448775126afc06c
Yosi Angel <yosi.angel@gmail.com>
2016-12-13 01:46:34 +0200

    6

--- src/CityInfo/Models/PointOfInterestForCreationDto.cs
+++ src/CityInfo/Models/PointOfInterestForCreationDto.cs
@@ -12,7 +12,7 @@ namespace CityInfo.API.Models
         [MaxLength(50)]
         public string Name { get; set; }
 
-        [MaxLength(200)]
+        [MaxLength(205)]
         public string Description { get; set; }
     }
 }
